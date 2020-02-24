cf16fc2d8d26f67a100481659310ecd37077399d
tabatagloria <tabatabruna@yahoo.com.br>
2020-02-24 17:12:53 -0300

    Implementando Classe Modelo

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hoteis = [
     {
@@ -48,18 +49,16 @@ class Hotel(Resource):
         return{'message': 'Hotel not found'}, 404
     
     def post(self, hotel_id):
-  
-
         dados = Hotel.argumentos.parse_args()
-
-        novo_hotel = { 'hotel_id': hotel_id, **dados }
-
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hoteis.append(novo_hotel)
         return novo_hotel, 200
     
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        novo_hotel = { 'hotel_id': hotel_id, **dados }
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
         hotel = Hotel.find_hotel(hotel_id)
         if hotel:
             hotel.update(novo_hotel)
