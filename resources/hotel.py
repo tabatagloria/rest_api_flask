b11a79af8e3d92a239298ac32847dc42077a2bfb
tabatagloria <tabatabruna@yahoo.com.br>
2020-02-24 16:33:16 -0300

    Implementando o Método DELETE

--- resources/hotel.py
+++ resources/hotel.py
@@ -68,4 +68,6 @@ class Hotel(Resource):
         return novo_hotel, 201
 
     def delete(self, hotel_id):
-        pass
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
+        return {'message': 'Hotel deleted'}
