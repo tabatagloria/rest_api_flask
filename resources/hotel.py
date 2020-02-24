c662aa424d7897664e03bf61caba45fe021d713e
tabatagloria <tabatabruna@yahoo.com.br>
2020-02-24 15:42:03 -0300

    Implementando o Método POST

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
     {
@@ -36,7 +36,24 @@ class Hotel(Resource):
         return{'message': 'Hotel not found'}, 404
     
     def post(self, hotel_id):
-        pass
+        argumentos = reqparse.RequestParser()
+        argumentos.add_argument('nome')
+        argumentos.add_argument('diaria')
+        argumentos.add_argument('cidade')
+        argumentos.add_argument('estrelas')
+
+        dados = argumentos.parse_args()
+
+        novo_hotel = {
+            'hotel_id': hotel_id,
+            'nome': dados['nome'],
+            'estrelas': dados['estrelas'],
+            'diaria': dados['diaria'],
+            'cidade': dados['cidade']
+        }
+
+        hoteis.append(novo_hotel)
+        return novo_hotel, 200
     
     def put(self, hotel_id):
         pass
