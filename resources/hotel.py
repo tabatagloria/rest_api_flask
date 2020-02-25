5d94a8271eee54801b1e9ae29f4989285b56e79f
tabatagloria <tabatabruna@yahoo.com.br>
2020-02-25 20:02:41 -0300

    Atualizando o Método DELETE

--- resources/hotel.py
+++ resources/hotel.py
@@ -63,6 +63,8 @@ class Hotel(Resource):
         return hotel.json(), 201
 
     def delete(self, hotel_id):
-        global hoteis
-        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
-        return {'message': 'Hotel deleted'}
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            hotel.delete_hotel()
+            return {'message': 'Hotel deleted'}
+        return {'message': 'Hotel not find.'}, 404
