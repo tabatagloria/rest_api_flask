3ed625eaa2e6f501ef02930ab3a92738524abaf4
tabatagloria <tabatabruna@yahoo.com.br>
2020-02-25 20:30:57 -0300

    Implementando Tratamento de Erros

--- resources/hotel.py
+++ resources/hotel.py
@@ -31,10 +31,10 @@ class Hoteis(Resource):
 
 class Hotel(Resource):
     argumentos = reqparse.RequestParser()
-    argumentos.add_argument('nome')
-    argumentos.add_argument('diaria')
-    argumentos.add_argument('cidade')
-    argumentos.add_argument('estrelas')
+    argumentos.add_argument('nome', type=str, required=True, help="The field 'nome' cannot be left blank"),
+    argumentos.add_argument('diaria', type=float, required=True, help="The field 'diária' cannot be left blank"),
+    argumentos.add_argument('cidade', type=str),
+    argumentos.add_argument('estrelas', type=float)
 
     def get(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
@@ -45,10 +45,12 @@ class Hotel(Resource):
     def post(self, hotel_id):
         if HotelModel.find_hotel(hotel_id):
             return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400
-
         dados = Hotel.argumentos.parse_args()
         hotel = HotelModel(hotel_id, **dados)
-        hotel.save_hotel()
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'An internal error ocurred trying to save hotel.'}, 500
         return hotel.json()
     
     def put(self, hotel_id):
@@ -59,12 +61,18 @@ class Hotel(Resource):
             hotel_encontrado.save_hotel()
             return hotel_encontrado.json(), 200
         hotel = HotelModel(hotel_id, **dados)
-        hotel.save_hotel()
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'An internal error ocurred trying to save hotel.'}, 500
         return hotel.json(), 201
 
     def delete(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            hotel.delete_hotel()
+            try:
+                hotel.delete_hotel()
+            except:
+                return {'message': 'An internal error ocurred trying to delete hotel.'}, 500
             return {'message': 'Hotel deleted'}
         return {'message': 'Hotel not find.'}, 404
