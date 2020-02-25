862a8fd96bfb5bff802800a2c09e07ddaad8896b
tabatagloria <tabatabruna@yahoo.com.br>
2020-02-25 19:38:30 -0300

    Atualizando o Método GET

--- resources/hotel.py
+++ resources/hotel.py
@@ -37,9 +37,9 @@ class Hotel(Resource):
     argumentos.add_argument('estrelas')
 
     def get(self, hotel_id):
-        hotel = Hotel.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            return hotel
+            return hotel.json()
         return{'message': 'Hotel not found'}, 404
     
     def post(self, hotel_id):
